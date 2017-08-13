a34d4640c31bd487a4fa629c8cee015118500134
Jason wang <qwang216@gmail.com>
2017-08-12 21:49:54 -0400

    something went wrong so i copied the files from completed project

--- library/app.js
+++ library/app.js
@@ -2,19 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.send('Hello World');
 });
 
 app.get('/books', function (req, res) {
-    res.send('hello books');
+    res.send('Hello Books');
 });
 
-app.listen(5000, function (err) {
+app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
