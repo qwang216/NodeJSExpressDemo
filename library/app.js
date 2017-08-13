8234b64cfe0be86448d65d18b2fa2139c92344fd
Jason wang <qwang216@gmail.com>
2017-08-13 01:15:01 -0400

    refactored bookRoutes out of app.js

--- library/app.js
+++ library/app.js
@@ -4,7 +4,7 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-var bookRouter = express.Router();
+var bookRouter = require('./src/routes/bookRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -12,78 +12,6 @@ app.set('views', './src/views');
 //var handlebars = require('express-handlebars');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', 'ejs');
-
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false
-    },
-    {
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: false
-    },
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        author: 'Jules Verne',
-        read: false
-    },
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false
-    },
-    {
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        author: 'Kenneth Grahame',
-        read: false
-    },
-    {
-        title: 'Life On The Mississippi',
-        genre: 'History',
-        author: 'Mark Twain',
-        read: false
-    },
-    {
-        title: 'Childhood',
-        genre: 'Biography',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    }
-    ];
-
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'Books',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            books: books
-        });
-    });
-
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello single Books');
-    });
-
 app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
