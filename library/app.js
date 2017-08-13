6cc1033c9fb29aff9d1496ba1ba99419bcd49ec9
Jason wang <qwang216@gmail.com>
2017-08-13 00:38:09 -0400

    added list of books to display at books view

--- library/app.js
+++ library/app.js
@@ -13,14 +13,76 @@ app.set('views', './src/views');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', 'ejs');
 
+var books = [
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    },
+    {
+        title: 'Les Misérables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: false
+    },
+    {
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: false
+    },
+    {
+        title: 'A Journey into the Center of the Earth',
+        genre: 'Science Fiction',
+        author: 'Jules Verne',
+        read: false
+    },
+    {
+        title: 'The Dark World',
+        genre: 'Fantasy',
+        author: 'Henry Kuttner',
+        read: false
+    },
+    {
+        title: 'The Wind in the Willows',
+        genre: 'Fantasy',
+        author: 'Kenneth Grahame',
+        read: false
+    },
+    {
+        title: 'Life On The Mississippi',
+        genre: 'History',
+        author: 'Mark Twain',
+        read: false
+    },
+    {
+        title: 'Childhood',
+        genre: 'Biography',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    }
+    ];
+
 bookRouter.route('/')
-    .get(function(req, res) {
-    res.send('Hello Books');
-});
+    .get(function (req, res) {
+        res.render('books', {
+            title: 'Books',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            }, {
+                Link: '/Authors',
+                Text: 'Authors'
+            }],
+            books: books
+        });
+    });
+
 bookRouter.route('/single')
-    .get(function(req, res) {
-    res.send('Hello single Books');
-});
+    .get(function (req, res) {
+        res.send('Hello single Books');
+    });
 
 app.use('/Books', bookRouter);
 
