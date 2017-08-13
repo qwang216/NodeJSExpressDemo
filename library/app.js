8139300cd237e55b773d07fb2322d0ee81963f95
Jason wang <qwang216@gmail.com>
2017-08-13 00:03:48 -0400

    using ejs as view src file

--- library/app.js
+++ library/app.js
@@ -5,14 +5,23 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.set('views','./src/views');
+app.set('views', './src/views');
 
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-app.set('view engine', 'hbs');  
+//var handlebars = require('express-handlebars');
+//app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render', list: ['a','b','c']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function (req, res) {
