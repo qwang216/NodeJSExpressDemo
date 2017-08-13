68d16f3be8b6885b8cbaa4c2e880c8e0d71c197e
Jason wang <qwang216@gmail.com>
2017-08-12 23:06:40 -0400

    using jade as view src

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -31,14 +31,14 @@ gulp.task('inject', function () {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
 
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 
-})
+});
 
 gulp.task('serve', ['style', 'inject'], function () {
     var options = {
@@ -48,10 +48,9 @@ gulp.task('serve', ['style', 'inject'], function () {
             'PORT': 3000
         },
         watch: jsFiles
-    }
-
+    };
     return nodemon(options)
         .on('restart', function (ev) {
-            console.log('Restarting....');
-        })
-})
+            console.log('Restarting......');
+        });
+});
