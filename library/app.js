6cf7f8b4a31523040e1b25c9e23b37adb699ea9e
Jason wang <qwang216@gmail.com>
2017-08-13 00:09:52 -0400

    added two book rounts

--- library/app.js
+++ library/app.js
@@ -4,12 +4,25 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
+var bookRouter = express.Router();
+
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 //var handlebars = require('express-handlebars');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function(req, res) {
+    res.send('Hello Books');
+});
+bookRouter.route('/single')
+    .get(function(req, res) {
+    res.send('Hello single Books');
+});
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
